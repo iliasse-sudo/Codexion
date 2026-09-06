04fda0a07e0bc79a2fd363e5cba50a184d9af095
Iliasse Baya <bayailiasse@gmail.com>
2026-09-06 12:01:18 +0100

    starting up

---
+++ includes/codexion.h
@@ -0,0 +1,23 @@
+#include <pthread.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <time.h>
+#include <unistd.h>
+#include <stdio.h>
+
+
+typedef struct s_coder
+{
+    /* data */
+} t_coder;
+
+typedef struct s_dongle
+{
+    /* data */
+} t_dongle;
+
+typedef struct s_sim
+{
+    /* data */
+} t_sim;
